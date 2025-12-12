4a3df8fa7e21787bc2a6af6671df1c4b5f38fba2
Areo-Joe <1047726015@qq.com>
2025-12-12 15:12:58 +0800

    chore: format

--- mcp/src/auth.ts
+++ mcp/src/auth.ts
@@ -1,38 +1,37 @@
-import { AuthSupevisor } from '@cloudbase/toolbox';
-import { debug } from './utils/logger.js';
+import { AuthSupevisor } from "@cloudbase/toolbox";
+import { debug } from "./utils/logger.js";
 
-const auth = AuthSupevisor.getInstance({
-})
+const auth = AuthSupevisor.getInstance({});
 
 export async function getLoginState() {
-    const {
-        TENCENTCLOUD_SECRETID,
-        TENCENTCLOUD_SECRETKEY,
-        TENCENTCLOUD_SESSIONTOKEN
-    } = process.env
-    debug('TENCENTCLOUD_SECRETID',TENCENTCLOUD_SECRETID)
-    if (TENCENTCLOUD_SECRETID && TENCENTCLOUD_SECRETKEY) {
-        debug('loginByApiSecret')
-        return {
-            secretId: TENCENTCLOUD_SECRETID,
-            secretKey: TENCENTCLOUD_SECRETKEY,
-            token: TENCENTCLOUD_SESSIONTOKEN
-        }
-        // await auth.loginByApiSecret(TENCENTCLOUD_SECRETID, TENCENTCLOUD_SECRETKEY, TENCENTCLOUD_SESSIONTOKEN)
-    }
-    
-    const loginState = await auth.getLoginState()
-    if (!loginState) {
-       await auth.loginByWebAuth({})
-       const loginState = await auth.getLoginState()
-       debug('loginByWebAuth',loginState)
-       return loginState
-    } else {
-        return loginState
-    }
+  const {
+    TENCENTCLOUD_SECRETID,
+    TENCENTCLOUD_SECRETKEY,
+    TENCENTCLOUD_SESSIONTOKEN,
+  } = process.env;
+  debug("TENCENTCLOUD_SECRETID", TENCENTCLOUD_SECRETID);
+  if (TENCENTCLOUD_SECRETID && TENCENTCLOUD_SECRETKEY) {
+    debug("loginByApiSecret");
+    return {
+      secretId: TENCENTCLOUD_SECRETID,
+      secretKey: TENCENTCLOUD_SECRETKEY,
+      token: TENCENTCLOUD_SESSIONTOKEN,
+    };
+    // await auth.loginByApiSecret(TENCENTCLOUD_SECRETID, TENCENTCLOUD_SECRETKEY, TENCENTCLOUD_SESSIONTOKEN)
+  }
+
+  const loginState = await auth.getLoginState();
+  if (!loginState) {
+    await auth.loginByWebAuth({});
+    const loginState = await auth.getLoginState();
+    debug("loginByWebAuth", loginState);
+    return loginState;
+  } else {
+    return loginState;
+  }
 }
 
 export async function logout() {
-    const result = await auth.logout()
-    return result
+  const result = await auth.logout();
+  return result;
 }
